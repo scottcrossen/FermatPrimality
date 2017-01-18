23d13d6f3e3b32a594f19c7852c098eb247b57b8
Scott Crossen <slxn42@byu.local>
2017-01-17 22:44:46 -0700

    initial commit

---
+++ Fermat Primality Tester/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Fermat_Primality_Tester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kvalue = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.solve = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Result";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Number of Tests";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Number to Test";
+            // 
+            // kvalue
+            // 
+            this.kvalue.Location = new System.Drawing.Point(37, 89);
+            this.kvalue.Name = "kvalue";
+            this.kvalue.Size = new System.Drawing.Size(210, 20);
+            this.kvalue.TabIndex = 16;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(37, 138);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(210, 53);
+            this.output.TabIndex = 17;
+            // 
+            // input
+            // 
+            this.input.AccessibleName = "";
+            this.input.Location = new System.Drawing.Point(37, 43);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(210, 20);
+            this.input.TabIndex = 15;
+            // 
+            // solve
+            // 
+            this.solve.Location = new System.Drawing.Point(107, 210);
+            this.solve.Name = "solve";
+            this.solve.Size = new System.Drawing.Size(75, 23);
+            this.solve.TabIndex = 14;
+            this.solve.Text = "Solve";
+            this.solve.UseVisualStyleBackColor = true;
+            this.solve.Click += new System.EventHandler(this.solve_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kvalue);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.solve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox kvalue;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button solve;
+    }
+}
+
---
+++ Fermat Primality Tester/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fermat_Primality_Tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void solve_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(input.Text); // Takes the input and number of test
+            int numtests = Convert.ToInt32(kvalue.Text); // And Stores them as ints, constant time complexity
+            if (number < 0)
+            {// If statements look for bad inputs, constant time
+                output.Text = "Please select a positive integer";
+            }
+            else if (numtests > number - 1)
+            {
+                output.Text = "K value should be less than n-1";
+            }
+            else
+            { // Begins valid input code
+                Boolean prime = true;
+                Random random = new Random(); // Generate random numbers for use as the base
+                for (int i = 1; i <= numtests; i++)
+                { // Primality checker, if we checked all numbers its O(n) time
+                    if (modexp(random.Next(2, number - 1), number - 1, number) != 1) prime = false;
+                }
+                if (prime == true)
+                {
+                    double p = 1.0;
+                    for (int i = 1; i <= numtests; i++)
+                    { // Calculates probability using
+                        p = (1.0 / 2.0) * p;
+                    }
+                    p = 100 * (1 - p);
+                    output.Text = "yes with probability " + p + "%";
+                }
+                else
+                { // Yes and no output given in the if/else
+                    output.Text = "no";
+                }
+            }
+        }
+        private long modexp(int x, int y, int N)
+        { // Modular Exponentiation O(n^3)
+            if (y == 0) return 1;
+            long z = modexp(x, Convert.ToInt32(Math.Floor(y / 2.0)), N);
+            if (y % 2 == 0) return (z * z) % N;
+            else return ((x % N) * ((z * z) % N)) % N;
+        }
+    }
+}
